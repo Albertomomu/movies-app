9f0470b6e1eacd83bd0ab6e986b032b7fb6b9903
Alberto <alberto.moreno2357@gmail.com>
2024-01-09 20:02:26 +0100

    fix: trying

--- src/components/moviesGrid.jsx
+++ src/components/moviesGrid.jsx
@@ -2,7 +2,7 @@ export const MoviesGrid = ({ movies }) => {
   const recMovies = movies.Search
   return (
     <div className='movies-container'>
-      {recMovies.map(movie => (
+      {recMovies && recMovies.map(movie => (
         <div className='movie' key={movie.imdbID}>
           <img src={movie.Poster} alt='' />
           <p>{movie.Title}</p>
